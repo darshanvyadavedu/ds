044c07294190cf58d992e66c14bd77d188a19149
Darshan <darshanvyadavedu@gmail.com>
2025-03-15 14:44:16 -0700

    Initial commit

---
+++ src/main/java/com/darshan/ds/slidingwindow/MinimumWindowSubstring76.java
@@ -0,0 +1,30 @@
+package com.darshan.ds.slidingwindow;
+
+public class MinimumWindowSubstring76 {
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+
+    }
+
+    public static String minWindow(String s, String t) {
+        int[] map = new int[128];
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+        int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
+        while (end < s.length()) {
+            if (map[s.charAt(end++)]-- > 0) {
+                counter--;
+            }
+            while (counter == 0) {
+                if (end - begin < d) {
+                    d = end - (head = begin);
+                }
+                if (map[s.charAt(begin++)]++ == 0) {
+                    counter++;
+                }
+            }
+        }
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
+    }
+}
---
+++ src/test/java/com/darshan/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darshan.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
